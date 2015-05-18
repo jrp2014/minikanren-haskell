b063b8bec05b608e05e3695576da822900da835b
Janik Riess <j.riess@wtnet.de>
2015-05-18 20:46:39 +0200

    unify

--- MiniKanren.hs
+++ MiniKanren.hs
@@ -16,3 +16,19 @@ walk subs (Var var) = case lookup var subs of
     Just term -> walk subs term
 walk _    other     = other
 
+unify :: Eq a => Substitution a -> Term a -> Term a -> Maybe (Substitution a)
+unify subs t u = case (walk subs t, walk subs u) of
+    (Var v, Var w) | v == w -> return subs
+    (Var v, term) -> return $ (v, term) : subs
+    (term, Var v) -> return $ (v, term) : subs
+    (List vs, List us) | length vs == length us ->
+        foldM (\subs' (v, w) -> unify subs' v w) subs $ zip vs us
+    (Data x, Data y) | x == y -> return subs
+    _ -> Nothing
+
+-- x == y -> [(x,y)]
+-- y == z -> [(x,y),(y,z)]
+-- z == w -> [(x,y),(y,z),(z,w)]
+-- w == x -> w == y -> w == z -> w == w -> [(x,y),(y,z),(z,w)]
+-- walk x
+
