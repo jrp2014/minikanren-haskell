48c697c04bdfb7147da49c0fcc4e548e01960dca
John Pavel <jrp@dial.pipex.com>
2018-10-14 12:13:34 +0100

    remove superfluous brackets

--- MiniKanren.hs
+++ MiniKanren.hs
@@ -1,27 +1,39 @@
-module MiniKanren (
-  LogicVar, Term(..), Environment(..),
-  Substitution, Goal, QueryProgram,
-  runAll, run, runStep,
-  fresh, freshs,
-  (===), (=/=),
-  conj, conde, condeDepthFirst,
-  (/\), (\/),
-  anyo
+module MiniKanren
+  ( LogicVar
+  , Term(..)
+  , Environment(..)
+  , Substitution
+  , Goal
+  , QueryProgram
+  , runAll
+  , run
+  , runStep
+  , fresh
+  , freshs
+  , (===)
+  , (=/=)
+  , conj
+  , conde
+  , condeDepthFirst
+  , (/\)
+  , (\/)
+  , emptyEnv
+  , anyo
   ) where
 
 import Control.Monad (foldM)
-import System.IO (hFlush, stdout)
 import Data.List (transpose)
+import System.IO (hFlush, stdout)
 
 -- | Logic variables are repesented as strings
 type LogicVar = String
 
 -- | a term is either a logic variable, some haskell data or a list of terms
 data Term a
-    = Var LogicVar
-    | Atom a
-    | List [Term a]
-    deriving (Eq, Read)
+  = Var LogicVar
+  | Atom a
+  | List [Term a]
+  deriving (Eq, Read)
 
 instance Show a => Show (Term a) where
   show (Var v) = '_' : v
@@ -30,14 +42,14 @@ instance Show a => Show (Term a) where
 
 -- | The state of the minikanren interpreter
 data Environment a = Environment
-    { counter :: Int -- ^ a count to create new intermediate variables
-    , substitution :: Substitution a -- ^ a substitution for evalues to be known equal
-    , disEqStore :: [Substitution a] -- ^ a list of substitutions for things to be known inequal
+  { counter :: Int -- ^ a count to create new intermediate variables
+  , substitution :: Substitution a -- ^ a substitution for evalues to be known equal
+  , disEqStore :: [Substitution a] -- ^ a list of substitutions for things to be known inequal
                                      --   (these are seperated because there might be not true at the same time
-    } deriving Show
+  } deriving (Show)
 
 emptyEnv :: Environment a
-emptyEnv = Environment {counter=0, substitution=[], disEqStore=[[]]}
+emptyEnv = Environment {counter = 0, substitution = [], disEqStore = [[]]}
 
 -- | A Substitution is a mapping from logic variables to other terms
 type Substitution a = [(LogicVar, Term a)]
@@ -53,10 +65,12 @@ type QueryProgram a = Term a -> Goal a
 
 -- | returns all values for a variable "q" in a query program and there inequalities
 runAll :: QueryProgram a -> [(Term a, [Term a])]
-runAll program = map (\ps ->
-        (reify (Var "q") (substitution ps),
-         map (flip walk $ Var "q") $ disEqStore ps)) $
-    program (Var "q") $ emptyEnv
+runAll program =
+  map
+    (\ps ->
+       ( reify (Var "q") (substitution ps)
+       , map (flip walk $ Var "q") $ disEqStore ps)) $
+  program (Var "q") emptyEnv
 
 -- | returns the first n values for a variable "q" in a query program and there inequalities
 run :: Int -> QueryProgram a -> [(Term a, [Term a])]
@@ -67,61 +81,77 @@ run solutions program = take solutions $ runAll program
 --   type anything else to exit
 runStep :: Show a => QueryProgram a -> IO Bool
 runStep program = loop (runAll program)
-    where
-        loop :: Show a => [a] -> IO Bool
-        loop [] = return False
-        loop (subs:subss) = do
-            putStr (show subs ++ " "); hFlush stdout
-            userAction <- getLine
-            case userAction of
-                "" -> loop subss
-                _ -> return True
+  where
+    loop :: Show a => [a] -> IO Bool
+    loop [] = return False
+    loop (subs:subss) = do
+      putStr (show subs ++ " ")
+      hFlush stdout
+      userAction <- getLine
+      case userAction of
+        "" -> loop subss
+        _ -> return True
 
 -- | creates a new logic variable and runs it inside a Goal
 fresh :: QueryProgram a -> Goal a
-fresh fn env = fn (Var $ show $ counter env) (env { counter = counter env + 1 })
+fresh fn env = fn (Var $ show $ counter env) (env {counter = counter env + 1})
 
 -- | creates n new logic variables and runs them inside a Goal
 freshs :: Int -> ([Term a] -> Goal a) -> Goal a
-freshs n fn env = fn (map (Var . show) [counter env..counter env + n-1])
-    (env { counter = counter env + n })
+freshs n fn env =
+  fn
+    (map (Var . show) [counter env .. counter env + n - 1])
+    (env {counter = counter env + n})
 
 -- | assert the equality of two terms, changes the local state (substitution)
 infix 4 ===
+
 (===) :: Eq a => Term a -> Term a -> Goal a
-(===) p q ps = case unify (substitution ps) p q of
-    Just subs | checkAssertions subs ps -> [ps { substitution = subs }]
+(===) p q ps =
+  case unify (substitution ps) p q of
+    Just subs
+      | checkAssertions subs ps -> [ps {substitution = subs}]
     _ -> []
 
 -- | try to get a substitution witch makes the given terms equal
 unify :: Eq a => Substitution a -> Term a -> Term a -> Maybe (Substitution a)
-unify subs t u = case (walk subs t, walk subs u) of
-    (Var v, Var w) | v == w -> return subs
+unify subs t u =
+  case (walk subs t, walk subs u) of
+    (Var v, Var w)
+      | v == w -> return subs
     (Var v, term) -> return $ (v, term) : subs
     (term, Var v) -> return $ (v, term) : subs
-    (List vs, List us) | length vs == length us ->
+    (List vs, List us)
+      | length vs == length us ->
         foldM (\subs' (v, w) -> unify subs' v w) subs $ zip vs us
-    (Atom x, Atom y) | x == y -> return subs
+    (Atom x, Atom y)
+      | x == y -> return subs
     _ -> Nothing
 
 -- | check if all inequalities are satisfied
 checkAssertions :: Eq a => Substitution a -> Environment a -> Bool
 checkAssertions subs ps = all exclude (disEqStore ps)
-    where exclude noSubs = all (\(var, _) ->
-              walk noSubs (Var var) /= walk subs (Var var)) noSubs
+  where
+    exclude noSubs =
+      all (\(var, _) -> walk noSubs (Var var) /= walk subs (Var var)) noSubs
 
 -- | assert the inequality of two terms, changes the local state (disEqStore)
 infix 4 =/=
+
 (=/=) :: Eq a => Term a -> Term a -> Goal a
-(=/=) p q ps = case unify (substitution ps) p q of
-                 Nothing -> return ps
-                 Just subs
+(=/=) p q ps =
+  case unify (substitution ps) p q of
+    Nothing -> return ps
+    Just subs
                     -- we fail here if they are already equal
                     -- so we dont need checkAssertions here
-                    | length subs == length (substitution ps) -> []
-                    | otherwise -> return $ ps { disEqStore =
-                            take (length subs - length (substitution ps)) subs
-                            : disEqStore ps }
+      | length subs == length (substitution ps) -> []
+      | otherwise ->
+        return $
+        ps
+          { disEqStore =
+              take (length subs - length (substitution ps)) subs : disEqStore ps
+          }
 
 -- | helper to create a logic operation bunch of conjugated logic operations
 conj :: [Goal a] -> Goal a
@@ -129,16 +159,18 @@ conj = foldr (/\) return
 
 -- | conjunction: combine two logic operations such that both are satisfied
 infixl 3 /\
+
 (/\) :: Goal a -> Goal a -> Goal a
 (/\) x y ps = concatMap y $ x ps
 
 -- | uses depth first search for disjunction
 condeDepthFirst :: [Goal a] -> Goal a
 condeDepthFirst = foldr condeDepthFirstSearch' (const [])
-    where
-      condeDepthFirstSearch' :: --forall a t.
-                            (t -> [a]) -> (t -> [a]) -> t -> [a]
-      condeDepthFirstSearch' x y ps = x ps ++ y ps
+  where
+    condeDepthFirstSearch' --forall a t.
+     ::
+         (t -> [a]) -> (t -> [a]) -> t -> [a]
+    condeDepthFirstSearch' x y ps = x ps ++ y ps
 
 -- | helper to create a logic operation bunch of disjunct logic operations
 conde :: [Goal a] -> Goal a
@@ -146,36 +178,39 @@ conde = foldr (\/) (const [])
 
 -- | disjunction: combines two logic operations such that one or both of them are satisfied
 infixl 2 \/
+
 (\/) :: Goal a -> Goal a -> Goal a
 --(\/) a b ps = (concat . transpose) [a ps,  b ps] -- fairer?
-(\/) a b ps = (a ps) ++ (b ps) -- depth first?
+(\/) a b ps = a ps ++ b ps -- depth first?
 
 anyo :: Goal a -> Goal a
 anyo g = conde $ repeat g
+
 --anyo g = conde [g, anyo g]
-
-
 -- | apply a substitution on a term, and replacing all variables by there equal terms
 walk :: Substitution a -> Term a -> Term a
-walk subs var@(Var v) = maybe var (walk subs) (lookup v subs)   
-walk _    term        = term
+walk subs var@(Var v) = maybe var (walk subs) (lookup v subs)
+walk _ term = term
 
 -- | every variable in the term is either substituted using the substitution or by a default substitution
 reify :: Term a -> Substitution a -> Term a
 reify v s = walkStar v' (reifyS v' [])
-    where v' = walkStar v s
+  where
+    v' = walkStar v s
 
 -- | walk a term with a substitution until walk dosen't produce new nested structures
 walkStar :: Term a -> Substitution a -> Term a
-walkStar v s = case v' of
+walkStar v s =
+  case v' of
     List terms -> List $ map (`walkStar` s) terms
     _ -> v'
-    where v' = walk s v
+  where
+    v' = walk s v
 
 -- | adds default a substitution for every variable in the term
 reifyS :: Term a -> Substitution a -> Substitution a
-reifyS v s = case walk s v of
+reifyS v s =
+  case walk s v of
     Var name -> (name, Var $ "_" ++ show (length s)) : s
     List terms -> foldl (flip reifyS) s terms
     Atom _ -> s
-
--- Test.hs
+++ Test.hs
@@ -5,27 +5,30 @@ import MiniKanren
 -- appendo
 appendo :: Eq a => Term a -> Term a -> Term a -> Goal a
 appendo xs ys res =
-    conde
-        [xs === List [] /\ ys === res,
-         freshs 3 $ \[car, cdr, rec] ->
-            conj [xs === List [car, cdr],
-                  res === List [car, rec],
-                  appendo cdr ys rec]]
+  conde
+    [ xs === List [] /\ ys === res
+    , freshs 3 $ \[car, cdr, rec] ->
+        conj
+          [xs === List [car, cdr], res === List [car, rec], appendo cdr ys rec]
+    ]
 
 testAppendo :: [(Term Int, [Term Int])]
-testAppendo = runAll $ \q -> freshs 3 $ \[a,b,c] ->
-    conj
-        [a === fromList [1,2,3],
-         b === fromList [4,5],
-         appendo a b c,
-         q === List [a,b,c]]
+testAppendo =
+  runAll $ \q ->
+    freshs 3 $ \[a, b, c] ->
+      conj
+        [ a === fromList [1, 2, 3]
+        , b === fromList [4, 5]
+        , appendo a b c
+        , q === List [a, b, c]
+        ]
 
 testBack1 :: [(Term Int, [Term Int])]
-testBack1 = runAll $ \a -> freshs 2 $ \[b,c] ->
-    conj
-        [c === fromList [1,2,3,4,5],
-         b === fromList [4,5],
-         appendo a b c]
+testBack1 =
+  runAll $ \a ->
+    freshs 2 $ \[b, c] ->
+      conj
+        [c === fromList [1, 2, 3, 4, 5], b === fromList [4, 5], appendo a b c]
 
 -- ns df search test
 nsDF :: Int -> Term Int -> Goal Int
@@ -40,24 +43,16 @@ ns n x = conde [x === Atom n, ns n x]
 testNumbers :: [(Term Int, [Term Int])]
 testNumbers = run 10 $ \x -> conde [ns 5 x, ns 6 x]
 
-
 -- rembero
 rembero :: Eq a => Term a -> Term a -> Term a -> Goal a
-rembero x ls out = conde
-    [conj
-        [ls === List [],
-         out === List []],
-     freshs 2 $ \[a,d] ->
-       conj
-        [ls === List [a, d],
-         a === x,
-         d === out],
-     freshs 3 $ \[a, d, res] ->
-       conj
-        [ls === List [a, d],
-         a =/= x,
-         out === List [a, res],
-         rembero x d res]]
+rembero x ls out =
+  conde
+    [ conj [ls === List [], out === List []]
+    , freshs 2 $ \[a, d] -> conj [ls === List [a, d], a === x, d === out]
+    , freshs 3 $ \[a, d, res] ->
+        conj
+          [ls === List [a, d], a =/= x, out === List [a, res], rembero x d res]
+    ]
 
 testRembero :: [(Term Int, [Term Int])]
 testRembero = runAll $ \q -> rembero (Atom 2) (fromList [1, 2, 3, 2, 4]) q
@@ -71,23 +66,32 @@ toList x = [x]
 fromList :: [a] -> Term a
 fromList = foldr (\x acc -> List [Atom x, acc]) (List [])
 
-emptyEnv :: Environment a
-emptyEnv = Environment {counter=0, substitution=[], disEqStore=[[]]}
+--emptyEnv :: Environment a
+--emptyEnv = Environment {counter = 0, substitution = [], disEqStore = [[]]}
 
 aAndB :: Goal String
-aAndB = 
-    (fresh (\a -> a === Atom "7")) /\
-    (fresh (\b -> (b === Atom "5") \/(b === Atom "6")))
+aAndB =
+  fresh (\a -> a === Atom "7") /\
+  fresh (\b -> (b === Atom "5") \/ (b === Atom "6"))
+
+test0 :: [Environment String]
+test0 = aAndB emptyEnv
 
 -- The followmg should all give  [(3,[_q])]
+test1 :: [(Term Int, [Term Int])]
 test1 = runAll $ \y -> freshs 2 $ \[x, z] -> x === z /\ Atom 3 === y
+
+test2 :: [(Term Int, [Term Int])]
 test2 = runAll $ \y -> freshs 2 $ \[x, z] -> conj [x === z, Atom 3 === y]
-test3 = runAll $ \q -> freshs 2 $ \[x, z] -> conj [x === z, Atom 3 === z,
-                                                   q === x]
-test4 = runAll $ \y -> (freshs 2 $ \[x, y] -> conj [Atom 4 === x,
-                                                    x === y]) /\
-                       Atom 3 === y
 
+test3 :: [(Term Int, [Term Int])]
+test3 =
+  runAll $ \q -> freshs 2 $ \[x, z] -> conj [x === z, Atom 3 === z, q === x]
 
-test6 = run 20 $ \q -> anyo (conde [ Atom 1 === q, Atom 2 === q,
-                                     Atom 3 === q])
+test4 :: [(Term Int, [Term Int])]
+test4 =
+  runAll $ \y ->
+    freshs 2 (\[x, y] -> conj [Atom 4 === x, x === y]) /\ Atom 3 === y
+
+test5 :: [(Term Int, [Term Int])]
+test5 = run 20 $ \q -> anyo (conde [Atom 1 === q, Atom 2 === q, Atom 3 === q])
